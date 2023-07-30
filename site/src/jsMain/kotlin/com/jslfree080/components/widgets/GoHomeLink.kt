d70991671edab51e47fa268871e320081b889e55
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-07-30 16:14:03 +0900

    Initial commit

---
+++ site/src/jsMain/kotlin/com/jslfree080/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package com.jslfree080.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
