90e66d43af8949abb050fa0dae8130bacba8ea2b
Jung Soo Lee <jslfree080@gmail.com>
2024-03-24 04:56:41 +0900

    Minor fix

--- site/src/jsMain/kotlin/com/jslfree080/models/Coding.kt
+++ site/src/jsMain/kotlin/com/jslfree080/models/Coding.kt
@@ -58,37 +58,37 @@ enum class Coding(
             "A command line tool that employs samtools to extract reads from a BAM file. It then generates plots displaying stacked bases at a designated position on a chromosome, with each base represented in a distinct color. It is currently unmaintained."
         )
     ),
-    Python(
-        icon = Res.Icon.python,
-        imageDesc = "",
-        title = "Python",
-        subtitle = listOf(
-            "GEOdownload.py",
-            "dataPreProcessing.ipynb"
-        ),
-        site = listOf(
-            "https://github.com/jslfree080/JslCodes/blob/main/GEOdownload.py",
-            "https://github.com/jslfree080/dataPreProcessing/blob/main/src/main/python/dataPreProcessing.ipynb"
-        ),
-        description = listOf(
-            "A simple Python script that automates the process of downloading supplementary files from GEO.",
-            "Notes on the fundamentals of data preprocessing in machine learning."
-        )
-    ),
-    Java(
-        icon = Res.Icon.java,
-        imageDesc = "",
-        title = "Java",
-        subtitle = listOf(
-            "VcfFormatParsedTxt.jar"
-        ),
-        site = listOf(
-            "https://github.com/jslfree080/JslCodes/blob/main/VcfFormatParsedTxt.jar"
-        ),
-        description = listOf(
-            "A command line tool that parses the FORMAT column of a VCF file and converts it into a TXT file."
-        )
-    ),
+//    Python(
+//        icon = Res.Icon.python,
+//        imageDesc = "",
+//        title = "Python",
+//        subtitle = listOf(
+//            "GEOdownload.py",
+//            "dataPreProcessing.ipynb"
+//        ),
+//        site = listOf(
+//            "https://github.com/jslfree080/JslCodes/blob/main/GEOdownload.py",
+//            "https://github.com/jslfree080/dataPreProcessing/blob/main/src/main/python/dataPreProcessing.ipynb"
+//        ),
+//        description = listOf(
+//            "A simple Python script that automates the process of downloading supplementary files from GEO.",
+//            "Notes on the fundamentals of data preprocessing in machine learning."
+//        )
+//    ),
+//    Java(
+//        icon = Res.Icon.java,
+//        imageDesc = "",
+//        title = "Java",
+//        subtitle = listOf(
+//            "VcfFormatParsedTxt.jar"
+//        ),
+//        site = listOf(
+//            "https://github.com/jslfree080/JslCodes/blob/main/VcfFormatParsedTxt.jar"
+//        ),
+//        description = listOf(
+//            "A command line tool that parses the FORMAT column of a VCF file and converts it into a TXT file."
+//        )
+//    ),
 //        R(
 //        icon = Res.Icon.r,
 //        imageDesc = "",
